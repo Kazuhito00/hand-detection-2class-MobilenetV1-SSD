b42389407b128ea37626067a9bf361b8abb2ff3e
takahashi <shigeki.takahashi.dev@gmail.com>
2020-04-08 02:19:03 +0900

    Updates

---
+++ precache-manifest.420779489e195f4cf1b4e207b8b38f0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02005b796549c4b7c416447694390c1",
+    "url": "/hand-detection-2class-MobilenetV1-SSD/index.html"
+  },
+  {
+    "revision": "17b4fdbbd05dac54ceea",
+    "url": "/hand-detection-2class-MobilenetV1-SSD/static/css/main.f9591bd5.chunk.css"
+  },
+  {
+    "revision": "2b1b0aa469b64bc37d7d",
+    "url": "/hand-detection-2class-MobilenetV1-SSD/static/js/2.7ee2a9c5.chunk.js"
+  },
+  {
+    "revision": "17b4fdbbd05dac54ceea",
+    "url": "/hand-detection-2class-MobilenetV1-SSD/static/js/main.6d15f44b.chunk.js"
+  },
+  {
+    "revision": "b038104941bf2091c01e",
+    "url": "/hand-detection-2class-MobilenetV1-SSD/static/js/runtime~main.cd4f2861.js"
+  }
+]);
